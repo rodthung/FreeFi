147878febec92cae672ffa3cdb268c296cbef1e8
Tank <r.sudrung@gmail.com>
2016-03-25 08:31:40 -0700

    add search box to the map

--- www/js/app.js
+++ www/js/app.js
@@ -5,18 +5,18 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'uiGmapgoogle-maps', 'app.controllers', 'app.routes', 'app.services', 'app.directives','ionic-material'])
+angular.module('app', ['ionic', 'uiGmapgoogle-maps', 'app.controllers', 'app.routes', 'app.services', 'app.directives', 'ionic-material'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,65 @@
 angular
-  .module('app.controllers', [])
+    .module('app.controllers', [])
 
-  .controller('freeFiCtrl', function ($scope) {
+.controller('freeFiCtrl', function ($scope) {
 
-  })
+})
 
-  .controller('loginCtrl', function ($scope) {
+.controller('loginCtrl', function ($scope) {
 
-  })
+})
 
-  .controller('signupCtrl', function ($scope) {
+.controller('signupCtrl', function ($scope) {
 
-  })
+})
 
-  .controller('mainCtrl', function ($scope) {
+.controller('mainCtrl', function ($scope) {
     navigator.geolocation.getCurrentPosition(function (position) {
-      $scope.map = {
-        center: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        },
-        zoom: 13
-      };
 
-      $scope.markersOptions = {
-        icon: '../img/connection.png'
-      };
+        var events = {
+            places_changed: function (searchBox) {}
+        }
+        $scope.searchbox = {
+            template: 'searchbox.tpl.html',
+            events: events
+        };
 
-      $scope.markers = [
-        {
-          id: 0,
-          coords: {
-            latitude: 40.1451,
-            longitude: -99.6680
-          }
+        $scope.map = {
+            center: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            },
+            zoom: 13
+        };
+
+        $scope.markersOptions = {
+            icon: '../img/connection.png'
+        };
+
+        $scope.markers = [
+            {
+                id: 0,
+                coords: {
+                    latitude: 40.1451,
+                    longitude: -99.6680
+                }
         },
-        {
-          id: 1,
-          coords: {
-            latitude: 38.1451,
-            longitude: -23.6680
-          }
+            {
+                id: 1,
+                coords: {
+                    latitude: 38.1451,
+                    longitude: -23.6680
+                }
         }
       ];
 
     });
-  })
+})
 
-  .controller('addWifiLocationCtrl', function ($scope) {
+.controller('addWifiLocationCtrl', function ($scope) {
 
-  })
+})
 
-  .controller('tutorialCtrl', function ($scope) {
+.controller('tutorialCtrl', function ($scope) {
 
-  })
+})
